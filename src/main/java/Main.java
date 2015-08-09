0a1441afbca441680b88cde4499ce728fbf16dcd
amoghcontact <amoghcontact@gmail.com>
2015-08-09 16:32:57 +0530

    commented hello and bye; deleted excel and test java files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,10 +20,10 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    /*get("/hello", (req, res) -> "Hello World");
     
     get("/bye", (req, res) -> "Bye World");
-
+*/
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
